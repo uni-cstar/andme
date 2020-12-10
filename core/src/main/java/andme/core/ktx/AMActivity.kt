3b8115575b71381fe7d2ea3bcf2567530bced239
Lucio <super_luo@163.com>
2020-12-10 22:40:45 +0800

    update

--- core/src/main/java/andme/core/ktx/AMActivity.kt
+++ core/src/main/java/andme/core/ktx/AMActivity.kt
@@ -1,11 +1,15 @@
 package andme.core.ktx
 
+import andme.core.R
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.util.TypedValue
+import androidx.annotation.AttrRes
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModelProvider
+
 
 /**
  * Created by Lucio on 2020-11-01.
@@ -17,8 +21,8 @@ import androidx.lifecycle.ViewModelProvider
  */
 fun <T : AndroidViewModel> FragmentActivity.obtainViewModel(clz: Class<T>): T {
     return ViewModelProvider(
-        this,
-        ViewModelProvider.AndroidViewModelFactory.getInstance(this.application)
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(this.application)
     ).get(clz)
 
 
@@ -34,3 +38,27 @@ fun Context.startActivity(clazz: Class<out Activity>) {
     startActivity(it)
 }
 
+
+/**
+ * 获取主题中的资源
+ * @return
+ */
+fun Context.getThemeAttrValue(@AttrRes attr:Int): Int {
+    val typedValue = TypedValue()
+    theme.resolveAttribute(attr, typedValue, true)
+    return typedValue.data
+}
+
+/**
+ * 获取背景色
+ */
+fun Context.getBackgroundColor():Int{
+    return getThemeAttrValue(R.attr.backgroundColor)
+}
+
+/**
+ * 获取主题色
+ */
+fun Context.getPrimaryColor():Int{
+    return getThemeAttrValue(R.attr.colorPrimary)
+}
