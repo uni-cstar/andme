101ffc17eb98f1cf1b2e8d59ae8f7d65e2a66edb
Lucio <super_luo@163.com>
2021-06-01 15:48:01 +0800

    banner新增gsy播放器测试版本

---
+++ tvarch/src/main/java/andme/tv/player/TVPlayer.kt
@@ -0,0 +1,13 @@
+package andme.tv.player
+
+import andme.tv.player.gsy.GsyPlayer
+
+/**
+ * Created by Lucio on 2021/6/1.
+ */
+object TVPlayer {
+
+    fun init(){
+       GsyPlayer.setup()
+    }
+}
