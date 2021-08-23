9a03e68949442ee8d1cd4bf0b7abf4e8f40c3947
Lucio <super_luo@163.com>
2021-08-23 15:37:06 +0800

    新增协议支持。支持打开二级页面协议

--- app/src/main/java/andme/sample/MainActivity.kt
+++ app/src/main/java/andme/sample/MainActivity.kt
@@ -1,7 +1,9 @@
 package andme.sample
 
+import andme.lang.urlEncode
 import android.content.ComponentName
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -15,10 +17,19 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<View>(R.id.btn).setOnClickListener {
             it.requestFocus()
-            val it =  Intent().also {
-                it.component = ComponentName("com.insight.hxytv", "ucux.android.tv.ui.entrance.LauncherActivity")
-                it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            }
+//            val it =  Intent().also {
+//                it.component = ComponentName("com.insight.hxytv", "ucux.android.tv.ui.entrance.LauncherActivity")
+//                it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//            }
+
+            //对应界面协议
+            val directTo = "gdtv://exam/errquestion"
+            //对协议进行url编码
+            val encodeDirectTo = directTo.urlEncode()
+            //将前面得到的值作为uri参数，封装成新的协议
+            val uri = Uri.parse("uxtv://applink?uri=${encodeDirectTo}")
+            //固定action和data创建意图
+            val it = Intent("android.intent.action.VIEW", uri)
             startActivity(it)
         }
     }
